9f4c1be0ddf152d51307709f5f725c7575339d8e
lingchen <138715540+lingchen2333@users.noreply.github.com>
2025-06-14 09:17:10 +0100

    remove chroma vector store and prepare for deployment

--- src/main/java/com/lingchen/livora/service/llm/ILLMService.java
+++
@@ -1,9 +0,0 @@
-package com.lingchen.livora.service.llm;
-
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
-
-public interface ILLMService {
-    String describeImage(MultipartFile image) throws IOException;
-}
